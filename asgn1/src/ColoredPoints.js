221765db1783e96f289e644d14cd10437adf47e8
Vivian Nguyen <109045145+iviang@users.noreply.github.com>
2026-01-18 02:41:46 -0800

    step 8

--- asgn1/src/ColoredPoints.js
+++ asgn1/src/ColoredPoints.js
@@ -28,7 +28,10 @@ function setupWebGL(){
   canvas = document.getElementById('webgl');
 
   // Get the rendering context for WebGL
-  gl = getWebGLContext(canvas);
+ // gl = getWebGLContext(canvas);
+
+  gl = canvas.getContext("webgl", { preserveDrawingBuffer: true});
+
   if (!gl) {
     console.log('Failed to get the rendering context for WebGL');
     return;
