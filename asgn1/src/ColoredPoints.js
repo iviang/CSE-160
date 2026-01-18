77f2fa82c419cf4670f5149daddabb2d645f5d9f
Vivian Nguyen <109045145+iviang@users.noreply.github.com>
2026-01-18 14:13:10 -0800

    reference drawing: inital

--- asgn1/src/ColoredPoints.js
+++ asgn1/src/ColoredPoints.js
@@ -271,42 +271,28 @@ function recreatePic() {
   //right antler
   insertTri([0.2, 0.25,    0.3, 0.3,   0.25, 0.4], DARK_BROWN); //a
   insertTri([0.1, 0.3,    0.2, 0.25,   0.15, 0.315], DARK_BROWN); //b
-
   insertTri([0.175, 0.4,    0.25, 0.4,   0.2, 0.5], DARK_BROWN); //c
-
   insertTri([0.175, 0.4,    0.25, 0.4,   0.2, 0.3], DARK_BROWN); //d
-
   insertTri([0.2, 0.5,    0.3, 0.5,   0.3, 0.3], DARK_BROWN); //e
-
   insertTri([0.3, 0.3,    0.4, 0.35,   0.3, 0.5], DARK_BROWN); //f
-
   insertTri([0.35, 0.4,    0.4, 0.35,   0.4, 0.6], DARK_BROWN); //g
-
   insertTri([0.4, 0.35,    0.5, 0.4,   0.4, 0.6], DARK_BROWN); //h
-
   insertTri([0.4, 0.6,    0.5, 0.6,   0.5, 0.4], DARK_BROWN); //i
 
   //left antler
 
 
   insertTri([-0.4, 0.3,    -0.2, 0.3,   -0.3, 0.25], DARK_BROWN); //c
-
   insertTri([-0.4, 0.3,    -0.3, 0.5,   -0.2, 0.3], DARK_BROWN); //d
-
   insertTri([-0.3, 0.5,    -0.4, 0.5,   -0.4, 0.3], DARK_BROWN); //e
-
   insertTri([-0.4, 0.3,    -0.5, 0.35,   -0.4, 0.5], DARK_BROWN); //f
-
   insertTri([-0.45, 0.4,   -0.5, 0.35,   -0.5, 0.6], DARK_BROWN); //g
-
   insertTri([-0.5, 0.35,    -0.6, 0.4,   -0.5, 0.6], DARK_BROWN); //h
-
   insertTri([-0.5, 0.6,    -0.6, 0.6,   -0.6, 0.4], DARK_BROWN); //i
 
-  //intials : VN 
+  //intials : VN ----
 
   insertTri([-0.3, 0.3,    -0.35, 0.4,   -0.25, 0.4], TAN); //V
-
   insertTri([-0.3, 0.375,    -0.35, 0.4,   -0.25, 0.4], DARK_BROWN); //V
 
 
@@ -316,8 +302,8 @@ function recreatePic() {
 
   insertTri([0.2, 0.25,    0.2, 0.3,   0.175, 0.4], TAN); //gap fill
   insertTri([0.1, 0.3,    0.185, 0.35,   0.15, 0.35], TAN); //gap fill
-  insertTri([0.1, 0.3,   0.15, 0.315,   0.15, 0.35], BROWN); //gap fill
-  insertTri([0.15, 0.315,   0.15, 0.35,   0.2, 0.25], BROWN); //gap fill
+  insertTri([0.1, 0.3,   0.15, 0.315,   0.15, 0.35], TAN); //gap fill
+  insertTri([0.15, 0.315,   0.15, 0.35,   0.2, 0.25], DARK_BROWN); //gap fill
 
 
 
@@ -347,9 +333,7 @@ function recreatePic() {
   insertTri([-0.1, -0.3,  0.0, -0.3,  0.0, -0.5], TAN); //17
   insertTri([-0.2, -0.5,   0.0, -0.5,   -0.1, -0.3], TAN); //18
   
-
   renderAllShapes();
-
 
 }
 
