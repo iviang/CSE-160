4f308cfaa7ecfdcc41b2950965ec0e14ec27ad4c
Vivian Nguyen <109045145+iviang@users.noreply.github.com>
2026-01-18 14:08:59 -0800

    reference drawing: inital

--- asgn1/src/ColoredPoints.js
+++ asgn1/src/ColoredPoints.js
@@ -287,8 +287,7 @@ function recreatePic() {
   insertTri([0.4, 0.6,    0.5, 0.6,   0.5, 0.4], DARK_BROWN); //i
 
   //left antler
-  // insertTri([0.2, 0.25,    0.3, 0.3,   0.25, 0.4], DARK_BROWN); //a
-  // insertTri([0.1, 0.3,    0.2, 0.25,   0.15, 0.315], DARK_BROWN); //b
+
 
   insertTri([-0.4, 0.3,    -0.2, 0.3,   -0.3, 0.25], DARK_BROWN); //c
 
@@ -304,12 +303,11 @@ function recreatePic() {
 
   insertTri([-0.5, 0.6,    -0.6, 0.6,   -0.6, 0.4], DARK_BROWN); //i
 
-
   //intial 
 
   insertTri([-0.3, 0.3,    -0.35, 0.4,   -0.25, 0.4], RED); //V
 
-  insertTri([-0.3, 0.375,    -0.35, 0.4,   -0.25, 0.4], RED); //V
+  insertTri([-0.3, 0.375,    -0.35, 0.4,   -0.25, 0.4], DARK_BROWN); //V
 
 
   insertTri([0.1, 0.3,    0.185, 0.35,   0.175, 0.4], RED); //N1
