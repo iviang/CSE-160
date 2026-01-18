466eeeb3a814f1ae9bebb6fb9aa5d743086df4bd
Vivian Nguyen <109045145+iviang@users.noreply.github.com>
2026-01-18 01:49:44 -0800

    step 6

--- asgn1/src/ColoredPoints.js
+++ asgn1/src/ColoredPoints.js
@@ -112,6 +112,31 @@ class Point{
     this.color = [1.0, 1.0, 1.0, 1.0];
     this.size = 5.0;
   }
+
+  render() {
+    var xy = this.position;
+    var rgba = this.color;
+    var size = this.size;
+    
+    // var xy = g_shapesList[i].position;
+    // var rgba = g_shapesList[i].color;
+    // var size = g_shapesList[i].size;
+
+    // var xy = g_points[i];
+    // var rgba = g_colors[i];
+    // var size = g_sizes[i];
+
+    // Pass the position of a point to a_Position variable
+    gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
+    // Pass the color of a point to u_FragColor variable
+    gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
+
+    // Pass the size of a point to u_Size variable
+    gl.uniform1f(u_Size, size);
+
+    // Draw the point
+    gl.drawArrays(gl.POINTS, 0, 1);
+  }
 }
 
 var g_shapesList = [];
@@ -172,23 +197,8 @@ function renderAllShapes(){
   var len = g_shapesList.length;
   
   for(var i = 0; i < len; i++) {
-    var xy = g_shapesList[i].position;
-    var rgba = g_shapesList[i].color;
-    var size = g_shapesList[i].size;
 
-    // var xy = g_points[i];
-    // var rgba = g_colors[i];
-    // var size = g_sizes[i];
+    g_shapesList.rendering();
 
-    // Pass the position of a point to a_Position variable
-    gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
-    // Pass the color of a point to u_FragColor variable
-    gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
-
-    // Pass the size of a point to u_Size variable
-    gl.uniform1f(u_Size, size);
-
-    // Draw the point
-    gl.drawArrays(gl.POINTS, 0, 1);
   }
 }
