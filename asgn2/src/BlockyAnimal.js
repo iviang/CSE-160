b10b97689ff06a6e75afd7c238bd2920f1673a7e
Vivian Nguyen <109045145+iviang@users.noreply.github.com>
2026-01-30 02:22:06 -0800

    set up

--- asgn2/src/BlockyAnimal.js
+++ asgn2/src/BlockyAnimal.js
@@ -146,7 +146,7 @@ function main() {
   // Register function (event handler) to be called on a mouse press
   canvas.onmousedown = click;
   //canvas.onmousemove = click;
-  
+
   canvas.onmousemove = function(ev) { if(ev.buttons == 1) { click(ev) } };
   // Specify the color for clearing <canvas>
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
@@ -176,12 +176,12 @@ function click(ev) { //transforms the coords from browser to canvas
     point = new Triangle();
   } else {
     point = new Circle();
-    point.segments = g_selectedSeg;
+    // point.segments = g_selectedSeg;
   }
   point.position=[x,y];
   point.color=g_selectedColor.slice();
   point.size=g_selectedSize;
-  g_redoList = [];
+//   g_redoList = [];
   g_shapesList.push(point);
 
 
@@ -211,11 +211,20 @@ function renderAllShapes() {
   gl.clear(gl.COLOR_BUFFER_BIT);
 
   //draw each shape in the list
-  var len = g_shapesList.length;
+    //   var len = g_shapesList.length;
 
-  for (var i = 0; i < len; i++) {
-    g_shapesList[i].render();
-  }
+    //   for (var i = 0; i < len; i++) {
+    //     g_shapesList[i].render();
+    //   }
+
+  //draw a test triangle
+  drawTriangle3D( [-1.0, 0.0, 0.0, -0.5,-1.0,0.0, 0.0,0.0,0.0]); 
+
+  //draw a cube
+  var body = new Cube();
+  body.color = [1.0,0.0,0.0,1.0];
+  body.render();
+
 
   //check the time at the end of the funciton, and show on web pg
   var duration = performance.now() - startTime;
@@ -233,168 +242,168 @@ function sendTextToHTML(text, htmlID) {
   htmlElm.innerHTML = text;
 }
 
-//color creation
-const BLACK = [0.0, 0.0, 0.0, 1.0];
-const DARK_BROWN = [0.35, 0.20, 0.10, 1.0];
-const BROWN = [0.55, 0.27, 0.07, 1.0];
-const TAN   = [0.65, 0.30, 0.20, 1.0]; //redish brown
-const PINK  = [1.0, 0.6, 0.6, 1.0];
+// //color creation
+// const BLACK = [0.0, 0.0, 0.0, 1.0];
+// const DARK_BROWN = [0.35, 0.20, 0.10, 1.0];
+// const BROWN = [0.55, 0.27, 0.07, 1.0];
+// const TAN   = [0.65, 0.30, 0.20, 1.0]; //redish brown
+// const PINK  = [1.0, 0.6, 0.6, 1.0];
 
-const RED = [1.0, 0.0, 0.0, 1.0];
+// const RED = [1.0, 0.0, 0.0, 1.0];
 
 //function to make the graphic of my reference
-function recreatePic() {
-  g_shapesList = []; //clear the page
-  gl.clear(gl.COLOR_BUFFER_BIT);
+// function recreatePic() {
+//   g_shapesList = []; //clear the page
+//   gl.clear(gl.COLOR_BUFFER_BIT);
 
-  // insertTri([-0.2, 0.0,  0.2, 0.0,  0.0, 0.3], [1, 0, 0, 1]); //TEST
+//   // insertTri([-0.2, 0.0,  0.2, 0.0,  0.0, 0.3], [1, 0, 0, 1]); //TEST
 
-  //left vert, right vert, top vert
+//   //left vert, right vert, top vert
 
-  //left eye
-  insertTri([-0.3, 0.0,   -0.2, 0.0,  -0.25, 0.05], BLACK); //top
-  insertTri([-0.3, 0.0,   -0.2, 0.0,  -0.25, -0.05], BLACK);
+//   //left eye
+//   insertTri([-0.3, 0.0,   -0.2, 0.0,  -0.25, 0.05], BLACK); //top
+//   insertTri([-0.3, 0.0,   -0.2, 0.0,  -0.25, -0.05], BLACK);
 
-  //right eye
-  insertTri([0.1, 0.0,  0.2, 0.0,  0.15, 0.05], BLACK); //top
-  insertTri([0.1, 0.0,  0.2, 0.0,  0.15, -0.05], BLACK);
+//   //right eye
+//   insertTri([0.1, 0.0,  0.2, 0.0,  0.15, 0.05], BLACK); //top
+//   insertTri([0.1, 0.0,  0.2, 0.0,  0.15, -0.05], BLACK);
 
-  //head ---
-  insertTri([-0.2, 0.0,   0.1, 0.0,  0.1, 0.3], BROWN); //1
-  insertTri([-0.2, 0.0,   0.1, 0.3,  -0.2, 0.3], BROWN); //2
+//   //head ---
+//   insertTri([-0.2, 0.0,   0.1, 0.0,  0.1, 0.3], BROWN); //1
+//   insertTri([-0.2, 0.0,   0.1, 0.3,  -0.2, 0.3], BROWN); //2
 
-  //left side of head
-  insertTri([-0.2, 0.0,    -0.25, 0.05,   -0.2, 0.3], BROWN); //3
-  insertTri([-0.25, 0.05,    -0.3, 0.0,   -0.2, 0.3], BROWN); //4
-  insertTri([-0.3, 0.0,    -0.4, 0.2,   -0.2, 0.3], BROWN); //5
-  insertTri([-0.3, 0.0,    -0.45, 0.1,   -0.4, 0.2], BROWN); //6
-  insertTri([-0.3, 0.0,    -0.45, -0.1,   -0.45, 0.1], BROWN); //7
-  insertTri([-0.35, -0.2,  -0.3, 0.0,  -0.45, -0.1], BROWN); //8
-  insertTri([-0.35, -0.2,  -0.3, 0.0,  -0.25, -0.05], BROWN); //9
-  insertTri([-0.35, -0.2,  -0.3, 0.0,  -0.35, -0.25], BROWN); //10
+//   //left side of head
+//   insertTri([-0.2, 0.0,    -0.25, 0.05,   -0.2, 0.3], BROWN); //3
+//   insertTri([-0.25, 0.05,    -0.3, 0.0,   -0.2, 0.3], BROWN); //4
+//   insertTri([-0.3, 0.0,    -0.4, 0.2,   -0.2, 0.3], BROWN); //5
+//   insertTri([-0.3, 0.0,    -0.45, 0.1,   -0.4, 0.2], BROWN); //6
+//   insertTri([-0.3, 0.0,    -0.45, -0.1,   -0.45, 0.1], BROWN); //7
+//   insertTri([-0.35, -0.2,  -0.3, 0.0,  -0.45, -0.1], BROWN); //8
+//   insertTri([-0.35, -0.2,  -0.3, 0.0,  -0.25, -0.05], BROWN); //9
+//   insertTri([-0.35, -0.2,  -0.3, 0.0,  -0.35, -0.25], BROWN); //10
 
-  //right side of head
-  insertTri([0.1, 0.0,    0.15, 0.05,   0.1, 0.3], BROWN); //3
-  insertTri([0.15, 0.05,    0.2, 0.0,   0.1, 0.3], BROWN); //4
-  insertTri([0.2, 0.0,    0.3, 0.2,   0.1, 0.3], BROWN); //5
-  insertTri([0.2, 0.0,    0.35, 0.1,   0.3, 0.2], BROWN); //6
-  insertTri([0.2, 0.0,    0.35, -0.1,   0.35, 0.1], BROWN); //7
-  insertTri([0.25, -0.2,  0.2, 0.0,  0.35, -0.1], BROWN); //8
-  insertTri([0.25, -0.2,  0.2, 0.0,  0.15, -0.05], BROWN); //9
-  insertTri([0.25, -0.2,  0.2, 0.0,  0.25, -0.25], BROWN); //10
+//   //right side of head
+//   insertTri([0.1, 0.0,    0.15, 0.05,   0.1, 0.3], BROWN); //3
+//   insertTri([0.15, 0.05,    0.2, 0.0,   0.1, 0.3], BROWN); //4
+//   insertTri([0.2, 0.0,    0.3, 0.2,   0.1, 0.3], BROWN); //5
+//   insertTri([0.2, 0.0,    0.35, 0.1,   0.3, 0.2], BROWN); //6
+//   insertTri([0.2, 0.0,    0.35, -0.1,   0.35, 0.1], BROWN); //7
+//   insertTri([0.25, -0.2,  0.2, 0.0,  0.35, -0.1], BROWN); //8
+//   insertTri([0.25, -0.2,  0.2, 0.0,  0.15, -0.05], BROWN); //9
+//   insertTri([0.25, -0.2,  0.2, 0.0,  0.25, -0.25], BROWN); //10
   
-  //ears ----
+//   //ears ----
 
-  //right ear
-  insertTri([0.2, 0.25,    0.3, 0.2,   0.4, 0.2], TAN); //a
-  insertTri([0.3, 0.2,   0.4, 0.2,   0.35, 0.1], TAN); //b
-  insertTri([0.35, 0.1,   0.4, 0.2,   0.5, 0.1], TAN); //c
-  insertTri([0.35, 0.1,   0.45, 0.0,   0.5, 0.1], TAN); //d
+//   //right ear
+//   insertTri([0.2, 0.25,    0.3, 0.2,   0.4, 0.2], TAN); //a
+//   insertTri([0.3, 0.2,   0.4, 0.2,   0.35, 0.1], TAN); //b
+//   insertTri([0.35, 0.1,   0.4, 0.2,   0.5, 0.1], TAN); //c
+//   insertTri([0.35, 0.1,   0.45, 0.0,   0.5, 0.1], TAN); //d
 
-  //left ear
-  insertTri([-0.3, 0.25,    -0.4, 0.2,   -0.5, 0.2], TAN); //a
-  insertTri([-0.4, 0.2,   -0.5, 0.2,   -0.45, 0.1], TAN); //b
-  insertTri([-0.45, 0.1,   -0.5, 0.2,   -0.6, 0.1], TAN); //c
-  insertTri([-0.45, 0.1,   -0.55, 0.0,   -0.6, 0.1], TAN); //d
+//   //left ear
+//   insertTri([-0.3, 0.25,    -0.4, 0.2,   -0.5, 0.2], TAN); //a
+//   insertTri([-0.4, 0.2,   -0.5, 0.2,   -0.45, 0.1], TAN); //b
+//   insertTri([-0.45, 0.1,   -0.5, 0.2,   -0.6, 0.1], TAN); //c
+//   insertTri([-0.45, 0.1,   -0.55, 0.0,   -0.6, 0.1], TAN); //d
 
-  //antler ---
+//   //antler ---
 
-  //right antler
-  insertTri([0.2, 0.25,    0.3, 0.3,   0.25, 0.4], DARK_BROWN); //a
-  insertTri([0.1, 0.3,    0.2, 0.25,   0.15, 0.315], DARK_BROWN); //b
-  insertTri([0.175, 0.4,    0.25, 0.4,   0.2, 0.5], DARK_BROWN); //c
-  insertTri([0.175, 0.4,    0.25, 0.4,   0.2, 0.3], DARK_BROWN); //d
-  insertTri([0.2, 0.5,    0.3, 0.5,   0.3, 0.3], DARK_BROWN); //e
-  insertTri([0.3, 0.3,    0.4, 0.35,   0.3, 0.5], DARK_BROWN); //f
-  insertTri([0.35, 0.4,    0.4, 0.35,   0.4, 0.6], DARK_BROWN); //g
-  insertTri([0.4, 0.35,    0.5, 0.4,   0.4, 0.6], DARK_BROWN); //h
-  insertTri([0.4, 0.6,    0.5, 0.6,   0.5, 0.4], DARK_BROWN); //i
+//   //right antler
+//   insertTri([0.2, 0.25,    0.3, 0.3,   0.25, 0.4], DARK_BROWN); //a
+//   insertTri([0.1, 0.3,    0.2, 0.25,   0.15, 0.315], DARK_BROWN); //b
+//   insertTri([0.175, 0.4,    0.25, 0.4,   0.2, 0.5], DARK_BROWN); //c
+//   insertTri([0.175, 0.4,    0.25, 0.4,   0.2, 0.3], DARK_BROWN); //d
+//   insertTri([0.2, 0.5,    0.3, 0.5,   0.3, 0.3], DARK_BROWN); //e
+//   insertTri([0.3, 0.3,    0.4, 0.35,   0.3, 0.5], DARK_BROWN); //f
+//   insertTri([0.35, 0.4,    0.4, 0.35,   0.4, 0.6], DARK_BROWN); //g
+//   insertTri([0.4, 0.35,    0.5, 0.4,   0.4, 0.6], DARK_BROWN); //h
+//   insertTri([0.4, 0.6,    0.5, 0.6,   0.5, 0.4], DARK_BROWN); //i
 
-  //left antler
+//   //left antler
 
-  insertTri([-0.4, 0.3,    -0.2, 0.3,   -0.3, 0.25], DARK_BROWN); //c
-  insertTri([-0.4, 0.3,    -0.3, 0.5,   -0.2, 0.3], DARK_BROWN); //d
-  insertTri([-0.3, 0.5,    -0.4, 0.5,   -0.4, 0.3], DARK_BROWN); //e
-  insertTri([-0.4, 0.3,    -0.5, 0.35,   -0.4, 0.5], DARK_BROWN); //f
-  insertTri([-0.45, 0.4,   -0.5, 0.35,   -0.5, 0.6], DARK_BROWN); //g
-  insertTri([-0.5, 0.35,    -0.6, 0.4,   -0.5, 0.6], DARK_BROWN); //h
-  insertTri([-0.5, 0.6,    -0.6, 0.6,   -0.6, 0.4], DARK_BROWN); //i
+//   insertTri([-0.4, 0.3,    -0.2, 0.3,   -0.3, 0.25], DARK_BROWN); //c
+//   insertTri([-0.4, 0.3,    -0.3, 0.5,   -0.2, 0.3], DARK_BROWN); //d
+//   insertTri([-0.3, 0.5,    -0.4, 0.5,   -0.4, 0.3], DARK_BROWN); //e
+//   insertTri([-0.4, 0.3,    -0.5, 0.35,   -0.4, 0.5], DARK_BROWN); //f
+//   insertTri([-0.45, 0.4,   -0.5, 0.35,   -0.5, 0.6], DARK_BROWN); //g
+//   insertTri([-0.5, 0.35,    -0.6, 0.4,   -0.5, 0.6], DARK_BROWN); //h
+//   insertTri([-0.5, 0.6,    -0.6, 0.6,   -0.6, 0.4], DARK_BROWN); //i
 
-  //intials : VN ----
+//   //intials : VN ----
 
-  insertTri([-0.3, 0.3,    -0.35, 0.4,   -0.25, 0.4], TAN); //V
-  insertTri([-0.3, 0.35,    -0.35, 0.4,   -0.25, 0.4], DARK_BROWN); //V
+//   insertTri([-0.3, 0.3,    -0.35, 0.4,   -0.25, 0.4], TAN); //V
+//   insertTri([-0.3, 0.35,    -0.35, 0.4,   -0.25, 0.4], DARK_BROWN); //V
 
 
-  insertTri([0.1, 0.3,    0.185, 0.35,   0.175, 0.4], TAN); //N1
-  insertTri([0.15, 0.35,    0.2, 0.25,   0.185, 0.35], TAN); //N2
-  insertTri([0.2, 0.25,    0.2, 0.3,   0.25, 0.4], TAN); //N3
+//   insertTri([0.1, 0.3,    0.185, 0.35,   0.175, 0.4], TAN); //N1
+//   insertTri([0.15, 0.35,    0.2, 0.25,   0.185, 0.35], TAN); //N2
+//   insertTri([0.2, 0.25,    0.2, 0.3,   0.25, 0.4], TAN); //N3
 
-  insertTri([0.2, 0.25,    0.2, 0.3,   0.175, 0.4], TAN); //gap fill
-  insertTri([0.1, 0.3,    0.185, 0.35,   0.15, 0.35], TAN); //gap fill
-  insertTri([0.1, 0.3,   0.15, 0.315,   0.15, 0.35], TAN); //gap fill
-  insertTri([0.15, 0.315,   0.15, 0.35,   0.2, 0.25], TAN); //gap fill
+//   insertTri([0.2, 0.25,    0.2, 0.3,   0.175, 0.4], TAN); //gap fill
+//   insertTri([0.1, 0.3,    0.185, 0.35,   0.15, 0.35], TAN); //gap fill
+//   insertTri([0.1, 0.3,   0.15, 0.315,   0.15, 0.35], TAN); //gap fill
+//   insertTri([0.15, 0.315,   0.15, 0.35,   0.2, 0.25], TAN); //gap fill
 
-  //nose ---
-  insertTri([0.0, 0.0,  0.1, 0.0,  0.0, -0.2], TAN); //1
-  insertTri([-0.1, -0.2,  0.0, -0.2,  0.0, 0.0], TAN); //2
-  insertTri([-0.2, 0.0,   0.0, 0.0,   -0.1, -0.2], TAN); //3
-  insertTri([-0.35, -0.2,   -0.1, -0.2,   -0.2, 0.0], TAN); //4
-  insertTri([-0.35, -0.2,   -0.2, 0.0,  -0.25, -0.05], TAN); //gap
-  insertTri([0.0, -0.2,   0.1, 0.0,   0.25, -0.2], TAN); //5
-  insertTri([0.1, 0.0,  0.15, -0.05,  0.25, -0.2], TAN); //gap
+//   //nose ---
+//   insertTri([0.0, 0.0,  0.1, 0.0,  0.0, -0.2], TAN); //1
+//   insertTri([-0.1, -0.2,  0.0, -0.2,  0.0, 0.0], TAN); //2
+//   insertTri([-0.2, 0.0,   0.0, 0.0,   -0.1, -0.2], TAN); //3
+//   insertTri([-0.35, -0.2,   -0.1, -0.2,   -0.2, 0.0], TAN); //4
+//   insertTri([-0.35, -0.2,   -0.2, 0.0,  -0.25, -0.05], TAN); //gap
+//   insertTri([0.0, -0.2,   0.1, 0.0,   0.25, -0.2], TAN); //5
+//   insertTri([0.1, 0.0,  0.15, -0.05,  0.25, -0.2], TAN); //gap
 
-  insertTri([-0.1, -0.3,   -0.1, -0.2,   -0.15, -0.25], PINK); //L nostril
-  insertTri([0.0, -0.3,   0.0, -0.2,   0.05, -0.25], PINK); //R nostril
+//   insertTri([-0.1, -0.3,   -0.1, -0.2,   -0.15, -0.25], PINK); //L nostril
+//   insertTri([0.0, -0.3,   0.0, -0.2,   0.05, -0.25], PINK); //R nostril
 
-  insertTri([-0.1, -0.2,  0.0, -0.2,  -0.1, -0.3], TAN); //6
-  insertTri([ 0.0, -0.2,  0.0, -0.3,  -0.1, -0.3], TAN); //7
-  insertTri([0.0, -0.2,   0.05, -0.25,   0.25, -0.2], TAN); //8
-  insertTri([-0.35, -0.2,   -0.1, -0.2,   -0.15, -0.25], TAN); //9
-  insertTri([-0.35, -0.3,   -0.1, -0.3,   -0.35, -0.2], TAN); //10
-  insertTri([-0.35, -0.2,   -0.1, -0.3,   -0.15, -0.25], TAN); //11
-  insertTri([0.0, -0.3,   0.25, -0.2,   0.05, -0.25], TAN); //12
-  insertTri([0.0, -0.3,   0.25, -0.2,   0.25, -0.3], TAN); //13
-  insertTri([-0.35, -0.3,   -0.1, -0.3,   -0.2, -0.5], TAN); //14
-  insertTri([0.0, -0.3,   0.1, -0.5,   0.25, -0.3], TAN); //15
-  insertTri([0.0, -0.5,  0.1, -0.5,  0.0, -0.3], TAN); //16
-  insertTri([-0.1, -0.3,  0.0, -0.3,  0.0, -0.5], TAN); //17
-  insertTri([-0.2, -0.5,   0.0, -0.5,   -0.1, -0.3], TAN); //18
+//   insertTri([-0.1, -0.2,  0.0, -0.2,  -0.1, -0.3], TAN); //6
+//   insertTri([ 0.0, -0.2,  0.0, -0.3,  -0.1, -0.3], TAN); //7
+//   insertTri([0.0, -0.2,   0.05, -0.25,   0.25, -0.2], TAN); //8
+//   insertTri([-0.35, -0.2,   -0.1, -0.2,   -0.15, -0.25], TAN); //9
+//   insertTri([-0.35, -0.3,   -0.1, -0.3,   -0.35, -0.2], TAN); //10
+//   insertTri([-0.35, -0.2,   -0.1, -0.3,   -0.15, -0.25], TAN); //11
+//   insertTri([0.0, -0.3,   0.25, -0.2,   0.05, -0.25], TAN); //12
+//   insertTri([0.0, -0.3,   0.25, -0.2,   0.25, -0.3], TAN); //13
+//   insertTri([-0.35, -0.3,   -0.1, -0.3,   -0.2, -0.5], TAN); //14
+//   insertTri([0.0, -0.3,   0.1, -0.5,   0.25, -0.3], TAN); //15
+//   insertTri([0.0, -0.5,  0.1, -0.5,  0.0, -0.3], TAN); //16
+//   insertTri([-0.1, -0.3,  0.0, -0.3,  0.0, -0.5], TAN); //17
+//   insertTri([-0.2, -0.5,   0.0, -0.5,   -0.1, -0.3], TAN); //18
   
-  renderAllShapes();
+//   renderAllShapes();
 
-}
+// }
 
-function insertTri(vertices, color) {
-  let t = new Triangle();
-  t.vertices = vertices;
-  t.color = color;
-  g_shapesList.push(t);
-}
+// function insertTri(vertices, color) {
+//   let t = new Triangle();
+//   t.vertices = vertices;
+//   t.color = color;
+//   g_shapesList.push(t);
+// }
 
-function downloadCanvas() {
-  const c = document.getElementById('webgl');
-  if (!c) return;
+// function downloadCanvas() {
+//   const c = document.getElementById('webgl');
+//   if (!c) return;
 
-  //blob implementation to do normal download
-  c.toBlob(function(blob) {
-    if (!blob) {
-      console.log("Canvas toBlob failed.");
-      return;
-    }
+//   //blob implementation to do normal download
+//   c.toBlob(function(blob) {
+//     if (!blob) {
+//       console.log("Canvas toBlob failed.");
+//       return;
+//     }
 
-  // const dataURL = c.toDataURL("image/png");
-  const url = URL.createObjectURL(blob);
-  const link = document.createElement('a');
-  const date = new Date().toISOString().split('T')[0];
+//   // const dataURL = c.toDataURL("image/png");
+//   const url = URL.createObjectURL(blob);
+//   const link = document.createElement('a');
+//   const date = new Date().toISOString().split('T')[0];
 
-  link.download = `asgn1_canvas${date}.png`;
+//   link.download = `asgn1_canvas${date}.png`;
 
-  link.href = url;
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
+//   link.href = url;
+//   document.body.appendChild(link);
+//   link.click();
+//   document.body.removeChild(link);
 
-  URL.revokeObjectURL(url);
-  }, "image/png");
-}
+//   URL.revokeObjectURL(url);
+//   }, "image/png");
+// }
