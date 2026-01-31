4d0f380d48c16735afe207e91d6dc8217502d18e
Vivian Nguyen <109045145+iviang@users.noreply.github.com>
2026-01-31 15:25:05 -0800

    editing cubes

--- asgn2/src/BlockyAnimal.js
+++ asgn2/src/BlockyAnimal.js
@@ -213,14 +213,14 @@ function renderAllShapes() {
   magenta.render();
 
   //a bunch of cubes rotating
-  var K=10.0;
-  for (var i=1; i<K; i++) {
-    var c = new Cube;
-    c.matrix.translate(-.8,1.9*i/K-1.0,0);
-    c.matrix.rotate(g_seconds*100,1,1,1);
-    c.matrix.scale(.1, 0.5/K, 1.0/K);
-    c.render();
-  }
+  // var K=10.0;
+  // for (var i=1; i<K; i++) {
+  //   var c = new Cube;
+  //   c.matrix.translate(-.8,1.9*i/K-1.0,0);
+  //   c.matrix.rotate(g_seconds*100,1,1,1);
+  //   c.matrix.scale(.1, 0.5/K, 1.0/K);
+  //   c.render();
+  // }
 
   //check the time at the end of the funciton, and show on web pg
   var duration = performance.now() - startTime;
