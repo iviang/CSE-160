e42a785d904e1c56a875ddcbcb8b036c7a4187ec
Vivian Nguyen <109045145+iviang@users.noreply.github.com>
2026-01-31 01:13:10 -0800

    slider

--- asgn2/src/BlockyAnimal.js
+++ asgn2/src/BlockyAnimal.js
@@ -110,7 +110,8 @@ function addActionsForHtmlUI(){
   document.getElementById('blueSlide').addEventListener('mouseup', function() { g_selectedColor[2] = this.value/100;  updatePreview(); });
 
   //size slider events
-  document.getElementById('angleSlide').addEventListener('mouseup', function() { g_globalAngle = this.value; renderAllShapes(); });
+  // document.getElementById('angleSlide').addEventListener('mouseup', function() { g_globalAngle = this.value; renderAllShapes(); });
+  document.getElementById('angleSlide').addEventListener('mousemove', function() { g_globalAngle = this.value; renderAllShapes(); });
 }
 
 function main() {
