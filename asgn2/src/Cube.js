c005cb218ff679a6439cdf99c89b312e4021b3a9
Vivian Nguyen <109045145+iviang@users.noreply.github.com>
2026-01-31 01:23:11 -0800

    slider

--- asgn2/src/Cube.js
+++ asgn2/src/Cube.js
@@ -17,29 +17,32 @@ class Cube{
         gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
 
         //front of the cube
-        drawTriangle3D( [0.0,0.0,0.0, 1.0,1.0,0.0, 1.0,0.0,0.0]);
-        drawTriangle3D( [0.0,0.0,0.0, 0.0,1.0,0.0, 1.0,1.0,0.0]);
+        drawTriangle3D( [0,0,0, 1,1,0, 1,0,0]);
+        drawTriangle3D( [0,0,0, 0,1,0, 1,1,0]);
+
+        //pass the color of a poiunt to u_FragColor uniform variable
+        gl.uniform4f(u_FragColor, rgba[0]*.9, rgba[1]*.9, rgba[2]*.9, rgba[3]);
 
         //other sides of cube top, bot, left, right, back
         //fill in yourself:
         //top
-        drawTriangle3D([0.0,1.0,0.0,  1.0,1.0,1.0,  1.0,1.0,0.0]);
-        drawTriangle3D([0.0,1.0,0.0,  0.0,1.0,1.0,  1.0,1.0,1.0]);
+        drawTriangle3D([0,1,0,  0,1,1,  1,1,1]);
+        drawTriangle3D([0,1,0,  1,1,1,  1,1,0]);
 
         //bot
-        drawTriangle3D([0.0,0.0,0.0,  1.0,0.0,0.0,  1.0,0.0,1.0]);
-        drawTriangle3D([0.0,0.0,0.0,  1.0,0.0,1.0,  0.0,0.0,1.0]);
+        drawTriangle3D([0,0,0,  1,0,0,  1,0,1]);
+        drawTriangle3D([0,0,0,  1,0,1,  0,0,1]);
 
         //left
-        drawTriangle3D([0.0,0.0,0.0,  0.0,1.0,1.0,  0.0,1.0,0.0]);
-        drawTriangle3D([0.0,0.0,0.0,  0.0,0.0,1.0,  0.0,1.0,1.0]);
+        drawTriangle3D([0,0,0,  0,1,1,  0,1,0]);
+        drawTriangle3D([0,0,0,  0,0,1,  0,1,1]);
 
         //right
-        drawTriangle3D([1.0,0.0,0.0,  1.0,1.0,0.0,  1.0,1.0,1.0]);
-        drawTriangle3D([1.0,0.0,0.0,  1.0,1.0,1.0,  1.0,0.0,1.0]);
+        drawTriangle3D([1,0,0,  1,1,0,  1,1,1]);
+        drawTriangle3D([1,0,0,  1,1,1,  1,0,1]);
 
         //back
-        drawTriangle3D([0.0,0.0,1.0,  1.0,0.0,1.0,  1.0,1.0,1.0]);
-        drawTriangle3D([0.0,0.0,1.0,  1.0,1.0,1.0,  0.0,1.0,1.0]);
+        drawTriangle3D([0,0,1,  1,0,1,  1,1,1]);
+        drawTriangle3D([0,0,1,  1,1,1,  0,1,1]);
     }
 }
