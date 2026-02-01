8fb171a47df75451357e0a7e7b27536a2ea2f11c
Vivian Nguyen <109045145+iviang@users.noreply.github.com>
2026-01-31 20:21:56 -0800

    making sphere

--- asgn2/src/Sphere.js
+++ asgn2/src/Sphere.js
@@ -4,6 +4,9 @@ class Sphere{
         this.color = [1.0, 1.0, 1.0, 1.0];
         this.matrix = new Matrix4();
 
+
+        this.latBands = 12;
+        this.longBands = 12;
     }
 
     render() {
@@ -17,44 +20,7 @@ class Sphere{
         //Pass tge natrix to u_ModelMatrix attibute
         gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
 
-        //front of the cube
-        drawTriangle3D( [0,0,0, 1,1,0, 1,0,0]);
-        drawTriangle3D( [0,0,0, 0,1,0, 1,1,0]);
-
-        //pass the color of a point to u_FragColor uniform variable
-        gl.uniform4f(u_FragColor, rgba[0]*.9, rgba[1]*.9, rgba[2]*.9, rgba[3]);
-
-        //other sides of cube top, bot, left, right, back
-        //fill in yourself:
-        //top
-        drawTriangle3D([0,1,0,  0,1,1,  1,1,1]);
-        drawTriangle3D([0,1,0,  1,1,1,  1,1,0]);
-
-        //pass the color of a poiunt to u_FragColor uniform variable
-        gl.uniform4f(u_FragColor, rgba[0]*.8, rgba[1]*.8, rgba[2]*.8, rgba[3]);
-
-        //bot
-        drawTriangle3D([0,0,0,  1,0,0,  1,0,1]);
-        drawTriangle3D([0,0,0,  1,0,1,  0,0,1]);
-
-        //pass the color of a point to u_FragColor uniform variable
-        gl.uniform4f(u_FragColor, rgba[0]*.7, rgba[1]*.7, rgba[2]*.7, rgba[3]);
-
-        //left
-        drawTriangle3D([0,0,0,  0,1,1,  0,1,0]);
-        drawTriangle3D([0,0,0,  0,0,1,  0,1,1]);
-
-        //pass the color of a point to u_FragColor uniform variable
-        gl.uniform4f(u_FragColor, rgba[0]*.6, rgba[1]*.6, rgba[2]*.6, rgba[3]);
-
-        //right
-        drawTriangle3D([1,0,0,  1,1,0,  1,1,1]);
-        drawTriangle3D([1,0,0,  1,1,1,  1,0,1]);
-
-        //pass the color of a point to u_FragColor uniform variable
-        gl.uniform4f(u_FragColor, rgba[0]*.5, rgba[1]*.5, rgba[2]*.5, rgba[3]);
-        //back
-        drawTriangle3D([0,0,1,  1,0,1,  1,1,1]);
-        drawTriangle3D([0,0,1,  1,1,1,  0,1,1]);
+        //draw sphere with triangles
+        
     }
 }
