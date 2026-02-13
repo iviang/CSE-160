fe12d448aa8d8f8b307168d506a137004843fa78
Vivian Nguyen <109045145+iviang@users.noreply.github.com>
2026-02-12 22:09:48 -0800

    loadTexture

--- asgn3/world/World.js
+++ asgn3/world/World.js
@@ -176,33 +176,39 @@ function addActionsForHtmlUI(){
 }
 
 
-function initTextures(gl, n) {
-  var texture = gl.createTexture();   // Create a texture object
-  if (!texture) {
-    console.log('Failed to create the texture object');
-    return false;
-  }
+function initTextures() {
+  // var texture = gl.createTexture();   // Create a texture object
+  // if (!texture) {
+  //   console.log('Failed to create the texture object');
+  //   return false;
+  // }
 
-  // Get the storage location of u_Sampler
-  var u_Sampler0 = gl.getUniformLocation(gl.program, 'u_Sampler0');
-  if (!u_Sampler0) {
-    console.log('Failed to get the storage location of u_Sampler0');
-    return false;
-  }
+  // // Get the storage location of u_Sampler
+  // var u_Sampler0 = gl.getUniformLocation(gl.program, 'u_Sampler0');
+  // if (!u_Sampler0) {
+  //   console.log('Failed to get the storage location of u_Sampler0');
+  //   return false;
+  // }
   var image = new Image();  // Create the image object
   if (!image) {
     console.log('Failed to create the image object');
     return false;
   }
   // Register the event handler to be called on loading an image
-  image.onload = function(){ loadTexture(gl, n, texture, u_Sampler0, image); };
+  image.onload = function(){ loadTexture(0, u_Sampler0, image); };
   // Tell the browser to load an image
   image.src = 'sky.jpg';
 
   return true;
 }
 
-function loadTexture(gl, n, texture, u_Sampler0, image) {
+function loadTexture(n, u_Sampler, image) {
+  var texture = gl.createTexture();
+  if (!texture) {
+    console.log('Failed to create the texture object');
+    return false;
+  }
+  
   gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1); // Flip the image's y axis
   // Enable texture unit0
   gl.activeTexture(gl.TEXTURE0);
@@ -215,11 +221,12 @@ function loadTexture(gl, n, texture, u_Sampler0, image) {
   gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
   
   // Set the texture unit 0 to the sampler
-  gl.uniform1i(u_Sampler0, 0);
+  gl.uniform1i(u_Sampler, 0);
   
-  gl.clear(gl.COLOR_BUFFER_BIT);   // Clear <canvas>
+  // gl.clear(gl.COLOR_BUFFER_BIT);   // Clear <canvas>
 
-  gl.drawArrays(gl.TRIANGLE_STRIP, 0, n); // Draw the rectangle
+  // gl.drawArrays(gl.TRIANGLE_STRIP, 0, n); // Draw the rectangle
+  console.log('Finished loadTexture');
 }
 
 
