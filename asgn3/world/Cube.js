1b1b17dd44accbfbbade52a0cba8ce107a3ffc2e
Vivian Nguyen <109045145+iviang@users.noreply.github.com>
2026-02-12 21:13:34 -0800

    3.1 - triangle uv adjustment

--- asgn3/world/Cube.js
+++ asgn3/world/Cube.js
@@ -26,34 +26,50 @@ class Cube{
         //other sides of cube top, bot, left, right, back
         //fill in yourself:
         //top
-        drawTriangle3D([0,1,0,  0,1,1,  1,1,1]);
-        drawTriangle3D([0,1,0,  1,1,1,  1,1,0]);
+        // drawTriangle3D([0,1,0,  0,1,1,  1,1,1]);
+        // drawTriangle3D([0,1,0,  1,1,1,  1,1,0]);
+        
+        drawTriangle3DUV( [0,1,0,  0,1,1,  1,1,1], [0,0, 0,1, 1,1] ); //new addition
+        drawTriangle3DUV( [0,1,0,  1,1,1,  1,1,0], [0,0, 1,1, 1,0] ); //new addition
 
         //pass the color of a poiunt to u_FragColor uniform variable
         gl.uniform4f(u_FragColor, rgba[0]*.8, rgba[1]*.8, rgba[2]*.8, rgba[3]);
 
         //bot
-        drawTriangle3D([0,0,0,  1,0,0,  1,0,1]);
-        drawTriangle3D([0,0,0,  1,0,1,  0,0,1]);
+        // drawTriangle3D([0,0,0,  1,0,0,  1,0,1]);
+        // drawTriangle3D([0,0,0,  1,0,1,  0,0,1]);
+
+        drawTriangle3DUV( [0,0,0,  1,0,0,  1,0,1], [0,0, 1,1, 1,0] ); //new addition
+        drawTriangle3DUV( [0,0,0,  1,0,1,  0,0,1], [0,0, 0,1, 1,1] ); //new addition
 
         //pass the color of a point to u_FragColor uniform variable
         gl.uniform4f(u_FragColor, rgba[0]*.7, rgba[1]*.7, rgba[2]*.7, rgba[3]);
 
         //left
-        drawTriangle3D([0,0,0,  0,1,1,  0,1,0]);
-        drawTriangle3D([0,0,0,  0,0,1,  0,1,1]);
+        // drawTriangle3D([0,0,0,  0,1,1,  0,1,0]);
+        // drawTriangle3D([0,0,0,  0,0,1,  0,1,1]);
 
+        drawTriangle3DUV( [0,0,0,  0,1,1,  0,1,0], [0,0, 1,1, 1,0] ); //new addition
+        drawTriangle3DUV( [0,0,0,  0,0,1,  0,1,1], [0,0, 0,1, 1,1] ); //new addition
+        
         //pass the color of a point to u_FragColor uniform variable
         gl.uniform4f(u_FragColor, rgba[0]*.6, rgba[1]*.6, rgba[2]*.6, rgba[3]);
 
         //right
-        drawTriangle3D([1,0,0,  1,1,0,  1,1,1]);
-        drawTriangle3D([1,0,0,  1,1,1,  1,0,1]);
+        // drawTriangle3D([1,0,0,  1,1,0,  1,1,1]);
+        // drawTriangle3D([1,0,0,  1,1,1,  1,0,1]);
+
+        drawTriangle3DUV( [1,0,0,  1,1,0,  1,1,1], [0,0, 1,1, 1,0] ); //new addition
+        drawTriangle3DUV( [1,0,0,  1,1,1,  1,0,1], [0,0, 0,1, 1,1] ); //new addition
 
         //pass the color of a point to u_FragColor uniform variable
         gl.uniform4f(u_FragColor, rgba[0]*.5, rgba[1]*.5, rgba[2]*.5, rgba[3]);
         //back
-        drawTriangle3D([0,0,1,  1,0,1,  1,1,1]);
-        drawTriangle3D([0,0,1,  1,1,1,  0,1,1]);
+        // drawTriangle3D([0,0,1,  1,0,1,  1,1,1]);
+        // drawTriangle3D([0,0,1,  1,1,1,  0,1,1]);
+        drawTriangle3DUV( [0,0,1,  1,0,1,  1,1,1], [0,0, 1,1, 1,0] ); //new addition
+        drawTriangle3DUV( [0,0,1,  1,1,1,  0,1,1], [0,0, 0,1, 1,1] ); //new addition
+
+
     }
 }
