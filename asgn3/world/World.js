bad7eb5216b513be5b38a9ee5b44f8305b1cd32b
Vivian Nguyen <109045145+iviang@users.noreply.github.com>
2026-02-13 05:19:17 -0800

    setting changes

--- asgn3/world/World.js
+++ asgn3/world/World.js
@@ -451,6 +451,30 @@ function keydown(ev) { //modify for the wasd keys
   renderAllShapes();
 }
 
+var g_map=[
+  [1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 1, 1, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 1, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 1], 
+];
+
+function drawMap() {
+  for (x=0;x<8;x++) {
+    for (y=0;y<8;y++) {
+      //console.log(x,y);
+      if (g_map[x][y] == 1) {
+        var body = new Cube();
+        body.color = [1.0, 1.0, 1.0, 1.0];
+        body.matrix.translate(x-4, -.75, y-4);
+        body.render();
+      }
+    }
+  }
+}
 
 function renderAllShapes() {
   //check the time at the start of this function
