80296130f27acd356fc8fb1528c45b9bc7bb3f93
Dillon Liang <vkieu.ngu@gmail.com>
2026-02-14 02:01:55 -0800

    efficiency

--- asgn3/world/Cube.js
+++ asgn3/world/Cube.js
@@ -45,8 +45,8 @@ class Cube{
         // drawTriangle3D([0,0,0,  1,0,0,  1,0,1]);
         // drawTriangle3D([0,0,0,  1,0,1,  0,0,1]);
 
-        drawTriangle3DUV( [0,0,0,  1,0,1,  1,0,0], [0,0, 1,0, 1,1] ); //new addition
-        drawTriangle3DUV( [0,0,0,  0,0,1,  1,0,1], [0,0, 0,1, 1,0] ); //new addition
+        drawTriangle3DUV( [0,0,0,  1,0,0,  1,0,1], [0,0, 1,0, 1,1] ); //new addition
+        drawTriangle3DUV( [0,0,0,  1,0,1,  0,0,1], [0,0, 0,1, 1,0] ); //new addition
 
         //pass the color of a point to u_FragColor uniform variable
         gl.uniform4f(u_FragColor, rgba[0]*.7, rgba[1]*.7, rgba[2]*.7, rgba[3]);
@@ -55,8 +55,8 @@ class Cube{
         // drawTriangle3D([0,0,0,  0,1,1,  0,1,0]);
         // drawTriangle3D([0,0,0,  0,0,1,  0,1,1]);
 
-        drawTriangle3DUV( [0,1,0,  0,1,1,  0,0,0], [0,0, 1,0, 1,1] ); //new addition
-        drawTriangle3DUV( [0,0,0,  0,1,1,  0,0,1], [0,0, 1,1, 0,1] ); //new addition
+        drawTriangle3DUV( [0,0,0,  0,1,1,  0,1,0], [0,0, 1,0, 1,1] ); //new addition
+        drawTriangle3DUV( [0,0,0,  0,0,1,  0,1,1], [0,0, 1,1, 0,1] ); //new addition
         
         //pass the color of a point to u_FragColor uniform variable
         gl.uniform4f(u_FragColor, rgba[0]*.6, rgba[1]*.6, rgba[2]*.6, rgba[3]);
@@ -65,17 +65,17 @@ class Cube{
         // drawTriangle3D([1,0,0,  1,1,0,  1,1,1]);
         // drawTriangle3D([1,0,0,  1,1,1,  1,0,1]);
 
-        drawTriangle3DUV( [1,1,0,  1,1,1,  1,0,0], [0,0, 1,0, 1,1] ); //new addition
+        drawTriangle3DUV( [1,0,0,  1,1,0,  1,1,1], [0,0, 1,0, 1,1] ); //new addition
         drawTriangle3DUV( [1,0,0,  1,1,1,  1,0,1], [0,0, 0,1, 1,0] ); //new addition
 
         //pass the color of a point to u_FragColor uniform variable
         gl.uniform4f(u_FragColor, rgba[0]*.5, rgba[1]*.5, rgba[2]*.5, rgba[3]);
-        
+
         //back
         // drawTriangle3D([0,0,1,  1,0,1,  1,1,1]);
         // drawTriangle3D([0,0,1,  1,1,1,  0,1,1]);
-        drawTriangle3DUV( [0,0,1,  1,1,1,  1,0,1], [0,0, 1,0, 1,1] ); //new addition
-        drawTriangle3DUV( [0,0,1,  0,1,1,  1,1,1], [0,0, 0,1, 1,0] ); //new addition
+        drawTriangle3DUV( [0,0,1,  1,0,1,  1,1,1], [0,0, 1,0, 1,1] ); //new addition
+        drawTriangle3DUV( [0,0,1,  1,1,1,  0,1,1], [0,0, 0,1, 1,0] ); //new addition
 
 
     }
@@ -101,20 +101,20 @@ class Cube{
         allverts=allverts.concat([0,1,0,  1,1,1,  1,1,0]);
 
         //bot
-        allverts=allverts.concat([0,0,0,  0,0,1,  1,0,1]);
-        allverts=allverts.concat([0,0,0,  1,0,1,  1,0,0]);
+        allverts=allverts.concat([0,0,0,  1,0,0,  1,0,1]);
+        allverts=allverts.concat([0,0,0,  1,0,1,  0,0,1]);
 
         //left
-        allverts=allverts.concat([0,1,0,  0,1,1,  0,0,0]);
-        allverts=allverts.concat([0,0,0,  0,1,1,  0,0,1]);
+        allverts=allverts.concat([0,0,0,  0,1,1,  0,1,0]);
+        allverts=allverts.concat([0,0,0,  0,0,1,  0,1,1]);
 
         //right
-        allverts=allverts.concat([1,1,0,  1,1,1,  1,0,0]);
+        allverts=allverts.concat([1,0,0,  1,1,0,  1,1,1]);
         allverts=allverts.concat([1,0,0,  1,1,1,  1,0,1]);
 
         //back
-        allverts=allverts.concat([0,0,1,  1,1,1,  1,0,1]);
-        allverts=allverts.concat([0,0,1,  0,1,1,  1,1,1]);
+        allverts=allverts.concat([0,0,1,  1,0,1,  1,1,1]);
+        allverts=allverts.concat([0,0,1,  1,1,1,  0,1,1]);
 
         drawTriangle3D(allverts);
     }
