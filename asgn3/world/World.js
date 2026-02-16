0cad6a2c03cd2b8f318154154bbfe1f53c72dbbb
Vivian Nguyen <vkieu.ngu@gmail.com>
2026-02-15 16:50:25 -0800

    cheese logics

--- asgn3/world/World.js
+++ asgn3/world/World.js
@@ -1065,9 +1065,9 @@ function renderAllShapes() {
   }
 
   //draw the RAT ==========
-  var rat = new Matrix4();
-  rat.translate(0, -.65, 0);
-  rat.scale(1,1,1);
+  // var rat = new Matrix4();
+  // rat.translate(0, -.65, 0);
+  // rat.scale(1,1,1);
   // g_rat.position = [0, -.65, 0];
   g_rat.render();
 
