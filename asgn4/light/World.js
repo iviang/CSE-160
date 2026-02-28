f46803942f103754b255f265742b29967153a6f9
Vivian Nguyen <109045145+iviang@users.noreply.github.com>
2026-02-28 03:59:26 -0800

    light

--- asgn4/light/World.js
+++ asgn4/light/World.js
@@ -680,7 +680,7 @@ function renderAllShapes() {
   body.color = [1.0, 0.0, 0.0, 1.0];
   body.textureNum=2;
   body.matrix.translate(0, -0.76, 0);
-  body.matrix.scale(5, 0.1, 5);
+  body.matrix.scale(10, 0.1, 10);
   body.matrix.translate(-0.5, 0, -0.5);
   body.render();
 
