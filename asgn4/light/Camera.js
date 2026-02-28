29fc43cee5668a5f883d5682375b33c81034d68a
Vivian Nguyen <109045145+iviang@users.noreply.github.com>
2026-02-28 05:36:46 -0800

    lighting:animation

--- asgn4/light/Camera.js
+++ asgn4/light/Camera.js
@@ -1,7 +1,7 @@
 class Camera{
     constructor(canvas){
         this.fov=60; //fov       
-        this.eye=new Vector3([0, .5, 2]); //eye
+        this.eye=new Vector3([0, .5, 2.4]); //eye
         this.at=new Vector3([0, .5, 0]); //at
         this.up=new Vector3([0,1,0]); //up
 
