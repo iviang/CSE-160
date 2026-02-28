e419913b89452d106da35f22ac599e043dbb4cdc
Vivian Nguyen <109045145+iviang@users.noreply.github.com>
2026-02-28 03:28:47 -0800

    sphere

--- asgn4/light/World.js
+++ asgn4/light/World.js
@@ -692,7 +692,7 @@ function renderAllShapes() {
   if (g_normalOn) sphere.textureNum=-3;
 
   sphere.matrix.translate(0, -.5, 0);
-  sphere.matrix.scale(0.5, 0.5, 1.5)
+  sphere.matrix.scale(0.5, 0.5, -1.5)
   sphere.render();
 
   var body = new Cube();
