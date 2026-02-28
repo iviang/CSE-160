edab50af379df149b0efaf848ef6ba3cf060cacf
Vivian Nguyen <109045145+iviang@users.noreply.github.com>
2026-02-27 23:57:37 -0800

    pass normals

--- asgn4/light/World.js
+++ asgn4/light/World.js
@@ -677,7 +677,7 @@ function renderAllShapes() {
   sky.color = [1.0, 0.0, 0.0, 1.0];
   if (g_normalOn) sky.textureNum=-3;
   sky.matrix.scale(5, 5, 5);
-  sky.matrix.translate(-0.5, -0.5, -0.5);
+  sky.matrix.translate(0.5, 0.5, 0.5);
   sky.render();
     
   // drawMap();
