3c307db0eee61f319cf85e9b415d00bd269419f8
Vivian Nguyen <109045145+iviang@users.noreply.github.com>
2026-02-28 03:41:25 -0800

    sphere

--- asgn4/light/Rat.js
+++ asgn4/light/Rat.js
@@ -192,7 +192,7 @@ class Rat {
     body.textureNum = -2;
     nose.color = PINK; 
     nose.matrix.set(snout2Coordinates); //connects to head
-    nose.matrix.translate(.8,0.05,0.4);
+    nose.matrix.translate(.8,0.05,0);
     nose.matrix.scale(.5, .3, .2);
     nose.render();
 
