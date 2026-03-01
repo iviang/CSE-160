afa42abb56942b0c4cb02ac53bc06f85238028a9
Vivian Nguyen <109045145+iviang@users.noreply.github.com>
2026-02-28 23:24:40 -0800

    spotlight

--- asgn4/light/World.js
+++ asgn4/light/World.js
@@ -107,12 +107,12 @@ var FSHADER_SOURCE = `
       vec3 Ls = normalize(u_spotlightPos - vec3(v_VertPos));
       vec3 D = -normalize(u_spotlightDir);
       float spotlightCos= dot(D, Ls);
+      float spotlightFactor = 0.0;
       if (spotlightCos >= u_spotlightCos) {
         spotlightFactor = pow(spotlightCos, u_spotlightExpo);
-      } else {
-        spotlightFactor = 0.0;
       }
-      gl_FragColor = vec4(vec3(diffuse + ambient + vec3(specular)) * spotFactor, 1.0);
+
+      gl_FragColor = vec4((diffuse + ambient + vec3(specular)) * spotFactor, 1.0);
     }
   }`
 
