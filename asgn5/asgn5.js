5dcdd13ede15d26601bc2ad3f56048b9ec254f28
Vivian Nguyen <109045145+iviang@users.noreply.github.com>
2026-03-11 10:07:36 -0700

    camera

--- asgn5/asgn5.js
+++ asgn5/asgn5.js
@@ -7,6 +7,8 @@ import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 function main() {
 
     const canvas = document.querySelector('#c');
+    // const view1Elem = document.querySelector('#view1');
+    // const view2Elem = document.querySelector('#view2');
     const renderer = new THREE.WebGLRenderer({antialias: true, canvas});
 
     // PerspectiveCamera
@@ -15,49 +17,67 @@ function main() {
     const near = 0.1;
     const far = 100;
     const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    // camera.position.z = 2;
 	camera.position.set( 0, 10, 20 );
-	const controls = new OrbitControls( camera, canvas );
-	controls.target.set( 0, 5, 0 );
-	controls.update();
 
-    // class MinMaxGUIHelper {
-    // constructor(obj, minProp, maxProp, minDif) {
-    //     this.obj = obj;
-    //     this.minProp = minProp;
-    //     this.maxProp = maxProp;
-    //     this.minDif = minDif;
-    // }
-    // get min() {
-    //     return this.obj[this.minProp];
-    // }
-    // set min(v) {
-    //     this.obj[this.minProp] = v;
-    //     this.obj[this.maxProp] = Math.max(this.obj[this.maxProp], v + this.minDif);
-    // }
-    // get max() {
-    //     return this.obj[this.maxProp];
-    // }
-    // set max(v) {
-    //     this.obj[this.maxProp] = v;
-    //     this.min = this.min;  // this will call the min setter
-    // }
-    // }
+    // const cameraHelper = new THREE.CameraHelper(camera);        
 
-    // function updateCamera() {
-    //     camera.updateProjectionMatrix();
-    // }
+    // MinMaxGUIHelper
+    class MinMaxGUIHelper {
+        constructor(obj, minProp, maxProp, minDif) {
+            this.obj = obj;
+            this.minProp = minProp;
+            this.maxProp = maxProp;
+            this.minDif = minDif;
+        }
+        get min() {
+            return this.obj[this.minProp];
+        }
+        set min(v) {
+            this.obj[this.minProp] = v;
+            this.obj[this.maxProp] = Math.max(this.obj[this.maxProp], v + this.minDif);
+        }
+        get max() {
+            return this.obj[this.maxProp];
+        }
+        set max(v) {
+            this.obj[this.maxProp] = v;
+            this.min = this.min;  // this will call the min setter
+        }
+    }
+
+    function updateCamera() {
+        camera.updateProjectionMatrix();
+    }
     
-    // const gui = new GUI();
-    // gui.add(camera, 'fov', 1, 180).onChange(updateCamera);
-    // const minMaxGUIHelper = new MinMaxGUIHelper(camera, 'near', 'far', 0.1);
-    // gui.add(minMaxGUIHelper, 'min', 0.1, 50, 0.1).name('near').onChange(updateCamera);
-    // gui.add(minMaxGUIHelper, 'max', 0.1, 50, 0.1).name('far').onChange(updateCamera);
+    const gui = new GUI();
+    gui.add(camera, 'fov', 1, 180).onChange(updateCamera);
+
+    const minMaxGUIHelper = new MinMaxGUIHelper(camera, 'near', 'far', 0.1);
+    gui.add(minMaxGUIHelper, 'min', 0.1, 50, 0.1).name('near').onChange(updateCamera);
+    gui.add(minMaxGUIHelper, 'max', 0.1, 50, 0.1).name('far').onChange(updateCamera);
+    const controls = new OrbitControls( camera, canvas );
+    controls.target.set( 0, 5, 0 );
+    controls.update();
+    // const camera2 = new THREE.PerspectiveCamera(
+	// 	60, // fov
+	// 	2, // aspect
+	// 	0.1, // near
+	// 	500, // far
+	// );
+	// camera2.position.set( 40, 10, 30 );
+	// camera2.lookAt( 0, 5, 0 );
+
+	// const controls2 = new OrbitControls( camera2, view2Elem );
+	// controls2.target.set( 0, 5, 0 );
+	// controls2.update(); 
+
 
     //Scene
     const scene = new THREE.Scene();
 	scene.background = new THREE.Color( 'black' );
+    // scene.add(cameraHelper);
 
+    
     //BoxGeometry
     const boxWidth = 1;
     const boxHeight = 1;
@@ -80,11 +100,6 @@ function main() {
     makeInstance(geometry, new THREE.MeshPhongMaterial({ color: 0x44aa88 }),-4,1,0);
     makeInstance(geometry, new THREE.MeshPhongMaterial({ color: 0x8844aa }),-2,1,0);
     makeInstance(geometry, new THREE.MeshPhongMaterial({ color: 0xaa8844 }), 0,1,0);
-
-    // solid color:
-    // const loader = new THREE.TextureLoader();
-    // const material = new THREE.MeshPhongMaterial({color: 0x44aa88});  // greenish blue
-    // const cube = new THREE.Mesh(geometry, material);
 
     //single Texture Loader
     {
@@ -143,25 +158,6 @@ function main() {
     const distance = halfSizeToFitOnScreen / Math.tan(halfFovY);
     }
     
-    // // compute a unit vector that points in the direction the camera is now
-    // // from the center of the box
-    // const direction = (new THREE.Vector3()).subVectors(camera.position, boxCenter).normalize();
-    
-    // // move the camera to a position distance units way from the center
-    // // in whatever direction the camera was from the center already
-    // camera.position.copy(direction.multiplyScalar(distance).add(boxCenter));
-    
-    // // pick some near and far values for the frustum that
-    // // will contain the box.
-    // camera.near = boxSize / 100;
-    // camera.far = boxSize * 100;
-    
-    // camera.updateProjectionMatrix();
-    
-    // // point the camera to look at the center of the box
-    // camera.lookAt(boxCenter.x, boxCenter.y, boxCenter.z);
-    // }
-
     //plane
 	{
 
@@ -169,10 +165,11 @@ function main() {
 
 		const loader = new THREE.TextureLoader();
 		const texture = loader.load( 'https://threejs.org/manual/examples/resources/images/checker.png' );
-		texture.colorSpace = THREE.SRGBColorSpace;
 		texture.wrapS = THREE.RepeatWrapping;
 		texture.wrapT = THREE.RepeatWrapping;
 		texture.magFilter = THREE.NearestFilter;
+   		texture.colorSpace = THREE.SRGBColorSpace;
+
 		const repeats = planeSize / 2;
 		texture.repeat.set( repeats, repeats );
 
@@ -213,14 +210,7 @@ function main() {
 	}
 
     //LIGHTS
-
-    //directional light
-    const color = 0xFFFFFF;
-    const intensity = 3;
-    const light = new THREE.DirectionalLight(color, intensity);
-    light.position.set(-1, 2, 4);
-    scene.add(light);
-
+    
 	{
 		const skyColor = 0xB1E1FF; // light blue
 		const groundColor = 0xB97A20; // brownish orange
@@ -229,11 +219,13 @@ function main() {
 		scene.add( light );
 	}
 
+    //directional
 	{
         const color = 0xFFFFFF;
         const intensity = 3;
         const light = new THREE.DirectionalLight( color, intensity );
         light.position.set( 5, 10, 2 );
+   		light.target.position.set( - 5, 0, 0 );
         scene.add( light );
         scene.add( light.target );
 	}
@@ -273,6 +265,7 @@ function main() {
             });
         });
 	}
+    
 
 	function resizeRendererToDisplaySize( renderer ) {
 		const canvas = renderer.domElement;
@@ -285,6 +278,30 @@ function main() {
 		return needResize;
 
 	}
+
+    // function setScissorForElement( elem ) {
+
+	// 	const canvasRect = canvas.getBoundingClientRect();
+	// 	const elemRect = elem.getBoundingClientRect();
+
+	// 	// compute a canvas relative rectangle
+	// 	const right = Math.min( elemRect.right, canvasRect.right ) - canvasRect.left;
+	// 	const left = Math.max( 0, elemRect.left - canvasRect.left );
+	// 	const bottom = Math.min( elemRect.bottom, canvasRect.bottom ) - canvasRect.top;
+	// 	const top = Math.max( 0, elemRect.top - canvasRect.top );
+
+	// 	const width = Math.min( canvasRect.width, right - left );
+	// 	const height = Math.min( canvasRect.height, bottom - top );
+
+	// 	// setup the scissor to only render to that part of the canvas
+	// 	const positiveYUpBottom = canvasRect.height - bottom;
+	// 	renderer.setScissor( left, positiveYUpBottom, width, height );
+	// 	renderer.setViewport( left, positiveYUpBottom, width, height );
+
+	// 	// return the aspect
+	// 	return width / height;
+
+	// }
 
     //animate
     function render(time) {
