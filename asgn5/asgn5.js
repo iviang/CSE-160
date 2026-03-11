035851149c2a2cf3e2025a5634f3d407cb6a725a
Vivian Nguyen <109045145+iviang@users.noreply.github.com>
2026-03-11 02:27:50 -0700

    obj loading

--- asgn5/asgn5.js
+++ asgn5/asgn5.js
@@ -2,6 +2,7 @@ import * as THREE from 'three';
 import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
 import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
 import {MTLLoader} from 'three/addons/loaders/MTLLoader.js';
+import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 
 function main() {
 
@@ -19,6 +20,39 @@ function main() {
 	const controls = new OrbitControls( camera, canvas );
 	controls.target.set( 0, 5, 0 );
 	controls.update();
+
+    class MinMaxGUIHelper {
+    constructor(obj, minProp, maxProp, minDif) {
+        this.obj = obj;
+        this.minProp = minProp;
+        this.maxProp = maxProp;
+        this.minDif = minDif;
+    }
+    get min() {
+        return this.obj[this.minProp];
+    }
+    set min(v) {
+        this.obj[this.minProp] = v;
+        this.obj[this.maxProp] = Math.max(this.obj[this.maxProp], v + this.minDif);
+    }
+    get max() {
+        return this.obj[this.maxProp];
+    }
+    set max(v) {
+        this.obj[this.maxProp] = v;
+        this.min = this.min;  // this will call the min setter
+    }
+    }
+
+    function updateCamera() {
+        camera.updateProjectionMatrix();
+    }
+    
+    const gui = new GUI();
+    gui.add(camera, 'fov', 1, 180).onChange(updateCamera);
+    const minMaxGUIHelper = new MinMaxGUIHelper(camera, 'near', 'far', 0.1);
+    gui.add(minMaxGUIHelper, 'min', 0.1, 50, 0.1).name('near').onChange(updateCamera);
+    gui.add(minMaxGUIHelper, 'max', 0.1, 50, 0.1).name('far').onChange(updateCamera);
 
     //Scene
     const scene = new THREE.Scene();
@@ -75,7 +109,29 @@ function main() {
         const progress = itemsLoaded / itemsTotal;
         progressBarElem.style.transform = `scaleX(${progress})`;
     };
-
+    function frameArea(sizeToFitOnScreen, boxSize, boxCenter, camera) {
+    const halfSizeToFitOnScreen = sizeToFitOnScreen * 0.5;
+    const halfFovY = THREE.MathUtils.degToRad(camera.fov * .5);
+    const distance = halfSizeToFitOnScreen / Math.tan(halfFovY);
+    
+    // compute a unit vector that points in the direction the camera is now
+    // from the center of the box
+    const direction = (new THREE.Vector3()).subVectors(camera.position, boxCenter).normalize();
+    
+    // move the camera to a position distance units way from the center
+    // in whatever direction the camera was from the center already
+    camera.position.copy(direction.multiplyScalar(distance).add(boxCenter));
+    
+    // pick some near and far values for the frustum that
+    // will contain the box.
+    camera.near = boxSize / 100;
+    camera.far = boxSize * 100;
+    
+    camera.updateProjectionMatrix();
+    
+    // point the camera to look at the center of the box
+    camera.lookAt(boxCenter.x, boxCenter.y, boxCenter.z);
+    }
     // const cube = new THREE.Mesh(geometry, materials);
 
     // function loadColorTexture( path ) {
@@ -112,6 +168,31 @@ function main() {
 
 	}
 
+    {
+
+		const cubeSize = 4;
+		const cubeGeo = new THREE.BoxGeometry( cubeSize, cubeSize, cubeSize );
+		const cubeMat = new THREE.MeshPhongMaterial( { color: '#8AC' } );
+		const mesh = new THREE.Mesh( cubeGeo, cubeMat );
+		mesh.position.set( cubeSize + 1, cubeSize / 2, 0 );
+		scene.add( mesh );
+
+	}
+
+	{
+
+		const sphereRadius = 3;
+		const sphereWidthDivisions = 32;
+		const sphereHeightDivisions = 16;
+		const sphereGeo = new THREE.SphereGeometry( sphereRadius, sphereWidthDivisions, sphereHeightDivisions );
+		const sphereMat = new THREE.MeshPhongMaterial( { color: '#CA8' } );
+		const mesh = new THREE.Mesh( sphereGeo, sphereMat );
+		mesh.position.set( - sphereRadius - 1, sphereRadius + 2, 0 );
+		scene.add( mesh );
+
+	}
+
+
 	{
 
 		const skyColor = 0xB1E1FF; // light blue
@@ -124,25 +205,25 @@ function main() {
 
 	{
 
-		const color = 0xFFFFFF;
-		const intensity = 3;
-		const light = new THREE.DirectionalLight( color, intensity );
-		light.position.set( 5, 10, 2 );
-		scene.add( light );
-		scene.add( light.target );
+        const color = 0xFFFFFF;
+        const intensity = 3;
+        const light = new THREE.DirectionalLight( color, intensity );
+        light.position.set( 5, 10, 2 );
+        scene.add( light );
+        scene.add( light.target );
 	}
     {
-    const mtlLoader = new MTLLoader();
-    mtlLoader.load('obj/materials.mtl', (mtl) => {
-        mtl.preload();
+        const mtlLoader = new MTLLoader();
+        mtlLoader.load('obj/materials.mtl', (mtl) => {
+            mtl.preload();
 
-        const objLoader = new OBJLoader();
-        objLoader.setMaterials(mtl);
-        objLoader.load('obj/model.obj', (root) => {
-        root.position.set(0, 1.6, 0);
-        root.scale.set(1, 1, 1); 
-        scene.add(root);
-        });
+            const objLoader = new OBJLoader();
+            objLoader.setMaterials(mtl);
+            objLoader.load('obj/model.obj', (root) => {
+            root.position.set(0, 1.6, 0);
+            root.scale.set(1, 1, 1); 
+            scene.add(root);
+            });
     });
 
 	}
